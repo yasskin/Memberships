23f548094ecb57ed8a1e6656f159d9193a6cea95
Noah Yasskin <noahyasskin@gmail.com>
2018-02-20 21:28:22 -0800

    Add Subscription Controller, Actions and Views

--- Memberships/Entities/Subscription.cs
+++ Memberships/Entities/Subscription.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Memberships.Entities
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [MaxLength(255)]
         [Required]
         public string Title { get; set; }
@@ -19,6 +21,7 @@ namespace Memberships.Entities
         [MaxLength(2048)]
         public string Description { get; set; }
 
+        [DisplayName("Registration Code")]
         [MaxLength(20)]
         public string RegistrationCode { get; set; }
     }
