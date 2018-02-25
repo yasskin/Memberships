9bfc1486a18ccec774a1bf5e28a0786984ac2864
Noah Yasskin <noahyasskin@gmail.com>
2018-02-24 20:53:38 -0800

    Add Admin Shared Paritals

---
+++ Memberships/Models/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserViewModel
+    {
+        [Display(Name = "User Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        [StringLength(30, ErrorMessage ="The {0} must be at least {1} characters long", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
